7dccadde9f1ac3894fc6ea87d9626d359c4074ec
Bianca Tang <tang.bianca13@gmail.com>
2020-02-12 15:37:41 -0800

    Added editing and deleting articles. Changed article list and article detail css

--- Articles/models.py
+++ Articles/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length = 100)
-    date = models.DateTimeField(auto_now_add = True)
-    author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default="[deleted]")
+    title = models.CharField(max_length=100)
+    dateP = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     body = models.TextField()
     slug = models.SlugField()
     picture = models.ImageField(default="defaultPic.png", blank="True")
+    dateE = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
--- Articles/views.py
+++ Articles/views.py
@@ -6,11 +6,11 @@ from . import forms
 
 # Create your views here.
 def article_list_view(request):
-    articles = Article.objects.all().order_by('date').reverse()
+    articles = Article.objects.all().order_by('dateP').reverse()
     return render(request, 'articles/article_list.html', {'articles':articles})
     
 def article_detail(request, slug):
-    article = Article.objects.get(slug = slug)
+    article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {"article":article})
 
 @login_required(login_url='/accounts/login/')
@@ -18,26 +18,35 @@ def create_article(request):
     if request.method == "POST":
         form = forms.ArticleForm(request.POST, request.FILES)
         if form.is_valid():
-            db_Entry = form.save(commit=False)
-            db_Entry.author = request.user
-            db_Entry.save()
-            return redirect('articles:articleDetail', slug=db_Entry.slug)
+            db_entry = form.save(commit=False)
+            db_entry.author = request.user
+            db_entry.save()
+            return redirect('articles:articleDetail', slug=db_entry.slug)
     else:
         form = forms.ArticleForm()
     return render(request, 'articles/create_article.html', {'form':form})
 
 @login_required(login_url='/accounts/login/')
 def edit_article(request):
-    db_Entry = Article.objects.get(slug = request.POST.get('slug'))
-    return render(request, 'articles/edit_article.html', {"article":db_Entry})
+    db_entry = Article.objects.get(slug=request.POST.get('slug'))
+    form = forms.ArticleEditForm(initial={'body':db_entry.body}, instance=db_entry)
+    return render(request, 'articles/edit_article.html', {"slug":db_entry.slug, "form":form})
+
+@login_required(login_url='/accounts/login/')
+def save_changes(request): 
+    slugLine = request.POST.get('editSlug')
+    db_entry = Article.objects.get(slug=slugLine)
+    if request.method == "POST":
+        authentication_form = forms.ArticleEditForm(request.POST, request.FILES, instance=db_entry)
+        if authentication_form.is_valid():
+            authentication_form.save()
+            return redirect('articles:articleDetail', slug=slugLine)
+    #if it doesn't work, then go back to the form page
+    return redirect('articles:editArticle')
 
 @login_required(login_url='/accounts/login/')
 def delete_article(request):
     if request.method == "POST":
-        db_Entry = Article.objects.get(slug = request.POST.get('slug'))
-        db_Entry.delete()
+        db_entry = Article.objects.get(slug=request.POST.get('slug'))
+        db_entry.delete()
     return redirect('articles:articles')
-
-@login_required(login_url='/accounts/login/')
-def save_changes(request): 
-    return HttpResponse("Updating post and saving changes!")
