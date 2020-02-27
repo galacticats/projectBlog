984ed340dc0887ce00cc461a5afe56c247dfedcd
Bianca Tang <tang.bianca13@gmail.com>
2020-02-27 13:23:42 -0800

    Added account page and account deletion. Still need to do CSS, passwordChange, and link to baseTemplate nav

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from django.apps import apps
 
 # Create your views here.
 def signup_view(request):
@@ -30,4 +33,33 @@ def login_view(request):
 def logout_view(request):
     if request.method == 'POST':
         logout(request)
+        return redirect('home')
+
+@login_required(login_url='/accounts/login/')
+def account_view(request):
+    account = User.objects.get(username=request.user)
+    uname = account.get_username()
+    Article = apps.get_model('Articles', 'Article')
+    articles = Article.objects.filter(author=account)
+    return render(request, 'accounts/accountPage.html', {'uname':uname, 'articles':articles})
+
+@login_required(login_url="/accounts/login/")
+def change_password_view(request):
+    if request.method == 'POST':
+        logout(request)
+        #TODO: change password here
+        return redirect('login')
+
+@login_required(login_url="/accounts/login/")
+def delete_account_view(request):
+    if request.method == 'POST':
+        account = User.objects.get(username=request.user)
+        logout(request)
+        #delete their articles
+        Article = apps.get_model('Articles', 'Article')
+        articles = Article.objects.filter(author=account)
+        for article in articles:
+            article.delete()
+        #then delete the user
+        account.delete()
         return redirect('home')
